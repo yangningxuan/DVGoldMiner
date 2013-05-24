69735ff28cf69a3117df05fc83d79db9d5bff2f7
user <user@Hackintosh-81.local>
2013-05-24 16:54:55 +0800

    Initial Commit

---
+++ DVGoldMiner/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  DVGoldMinerAppController.h
+//  DVGoldMiner
+//
+//  Created by user on 13-5-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ DVGoldMiner/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  DVGoldMinerAppController.h
+//  DVGoldMiner
+//
+//  Created by user on 13-5-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
